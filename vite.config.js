af9416e38f20bdbbf174c71f735da9b5b8b2226b
tuhinArmoy <tuhin.itsd@goldenharvestbd.com>
2025-12-13 19:54:00 +0600

    config updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/professor-search-based/',
   plugins: [react()],
   server: {
     port: 3000,
@@ -13,4 +14,3 @@ export default defineConfig({
     }
   }
 })
-
